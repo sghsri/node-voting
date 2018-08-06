bca09dd7b5b6cf366b195afce6bb4b7e77659381
Sriram Hariharan <sghsri@gmail.com>
2018-08-06 13:42:58 -0500

    questions and answers

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const app = express();
 var rooms = [];
+var bodyParser = require('body-parser');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });  
+
+app.use(bodyParser.json());
+
 app.get('/',(req, res) =>{
     res.sendFile(__dirname + '/index.html')
 });
+app.get('/create/',(req, res) => {
+    res.sendFile(__dirname + '/create.html');
+});
 app.post('/create',(req, res) => {
+    var party = req.body;
+    console.log(party);
     var roomid = generateRoomID();
-    rooms.push({id: roomid, participants: 1});
+    rooms.push({id: roomid, vote: party});
     console.log(rooms);
-    res.send(`Successfully created a party with id: ${roomid}`)
+    res.send(roomid)
     console.log(`Successfully created a party with id: ${roomid}`)
 });
 
@@ -24,9 +33,7 @@ app.get('/join/:id',(req, res) => {
     if(!theroom){
         return res.status(404).send(`Could not find a room with id ${roomid}`);
     } else {
-        theroom.participants++;
-        console.log(`User successfully joined a party with id: ${roomid}`)
-        res.sendFile(__dirname + '/party.html')
+        res.sendFile(__dirname + '/party.html');
     }
     console.log(rooms);
 });

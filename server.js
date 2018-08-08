923cb622692265e4ab5f99b2ff9932a0c64313d5
Sriram Hariharan <sghsri@gmail.com>
2018-08-08 11:50:11 -0500

    party edit

--- server.js
+++ server.js
@@ -1,87 +1,96 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 var rooms = [];
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
-app.get('/create/', (req, res) => {
-    res.sendFile(__dirname + '/create.html');
+app.get("/create/", (req, res) => {
+  res.sendFile(__dirname + "/create.html");
 });
-app.get('/party/:id', (req, res) => {
-    res.sendFile(__dirname + '/party.html');
+app.get("/party/", (req, res) => {
+  res.sendFile(__dirname + "/party.html");
 });
-app.get('/host/:id', (req, res) => {
-    res.sendFile(__dirname + '/host.html');
-});
-
-app.get('/api/party/:id', (req, res) => {
-    var roomid = req.params.id;
-    var theroom = rooms.find(x => { return x.id == roomid });
-    if (!theroom) {
-        return res.status(404).send(`Could not find a room with id ${roomid}`);
-    } else {
-        res.send(JSON.stringify(theroom));
-    }
+app.get("/host/:id", (req, res) => {
+  res.sendFile(__dirname + "/host.html");
 });
 
-
-app.post('/create', (req, res) => {
-    var party = req.body;
-    console.log(party);
-    var roomid = generateRoomID();
-    rooms.push({ id: roomid, vote: party });
-    console.log(rooms);
-    res.send(roomid)
-    console.log(`Successfully created a party with id: ${roomid}`)
+app.get("/api/party/:id", (req, res) => {
+  var roomid = req.params.id;
+  var theroom = rooms.find(x => {
+    return x.id == roomid;
+  });
+  if (!theroom) {
+    return res.status(404).send(`Could not find a room with id ${roomid}`);
+  } else {
+    res.send(JSON.stringify(theroom));
+  }
 });
 
-app.post('/join/:id', (req, res) => {
-    var roomid = req.params.id;
-    var theroom = rooms.find(x => { return x.id == roomid });
-    if (!theroom) {
-        return res.status(404).send(`Could not find a room with id ${roomid}`);
-    } else {
-        res.send(theroom);
-    }
+app.post("/create", (req, res) => {
+  var party = req.body;
+  console.log(party);
+  var roomid = generateRoomID();
+  rooms.push({ id: roomid, vote: party });
+  console.log(rooms);
+  res.send(roomid);
+  console.log(`Successfully created a party with id: ${roomid}`);
 });
-app.put('/party/:id', (req, res) => {
-    var party = req.body;
-    console.log(party.vote.answers);
-    var foundIndex = rooms.findIndex(x => x.id == req.params.id);
-    rooms[foundIndex] = party;
-    res.send("updated");
-    console.log(rooms[foundIndex].vote.answers);
+
+app.post("/join/:id", (req, res) => {
+  var roomid = req.params.id;
+  var theroom = rooms.find(x => {
+    return x.id == roomid;
+  });
+  if (!theroom) {
+    return res.status(404).send(`Could not find a room with id ${roomid}`);
+  } else {
+    res.send(theroom);
+  }
+});
+app.put("/party/:id", (req, res) => {
+  var party = req.body;
+  console.log(party.vote.answers);
+  var foundIndex = rooms.findIndex(x => x.id == req.params.id);
+  rooms[foundIndex] = party;
+  res.send("updated");
+  console.log(rooms[foundIndex].vote.answers);
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
+  console.log(`Listening on port: ${port}`);
 });
 
 function generateRoomID() {
-    var roomid = getIDString();
-    while (!isUniqueID(roomid)) {
-        roomid = getIDString();
-    }
-    return roomid;
+  var roomid = getIDString();
+  while (!isUniqueID(roomid)) {
+    roomid = getIDString();
+  }
+  return roomid;
 }
 function getIDString() {
-    return Math.random().toString(36).slice(-6).toLowerCase();
+  return Math.random()
+    .toString(36)
+    .slice(-6)
+    .toLowerCase();
 }
 function isUniqueID(roomid) {
-    for (var i = 0; i < rooms.length; i++) {
-        if (rooms[i].id == roomid) {
-            return false;
-        }
+  for (var i = 0; i < rooms.length; i++) {
+    if (rooms[i].id == roomid) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
